2559d29afe9bb06c994d2dadf533a6fd7f21d7ac
Sakila Sanharsha <sakila.sanharsha2k19@gmail.com>
2024-11-06 16:36:51 +0530

    add search

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -19,24 +19,48 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${GITHUB_URL}/users`, {
+  // Get search Results
+
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const params = new URLSearchParams({ q: text });
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
 
-    const data = await response.json();
+    const { items } = await response.json();
     dispatch({
       type: "GET_USERS",
-      payload: data,
+      payload: items,
     });
-
-    console.log(data);
   };
+
+  // get inital users (testing purposes)
+  // const fetchUsers = async () => {
+  //   setLoading();
+
+  //   const response = await fetch(`${GITHUB_URL}/users`, {
+  //     headers: {
+  //       Authorization: `token ${GITHUB_TOKEN}`,
+  //     },
+  //   });
+
+  //   const data = await response.json();
+  //   dispatch({
+  //     type: "GET_USERS",
+  //     payload: data,
+  //   });
+  // };
+
+  //set loading
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
+
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{ users: state.users, loading: state.loading, searchUsers }}
     >
       {children}
     </GithubContext.Provider>
